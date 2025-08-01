74f67e9d8158fced35877563c832896260fe3579
Shalaka Gajanayake <shalakagajanayake123@gmail.com>
2025-08-01 19:52:41 +0530

    footer.php - logo updated

--- pages/footer/footer.php
+++ pages/footer/footer.php
@@ -3,7 +3,7 @@
         <div class="row align-items-center">
             <div class="col-lg-3 col-md-4">
                 <div class="logo-footer">
-                    <a href="index.html">
+                    <a href="index.php">
                         <img src="assets/images/logo_light.svg" alt="">
                     </a>
                 </div>
