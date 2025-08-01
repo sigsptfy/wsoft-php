3ef1fbd6b463de883284279630d04ac81772823a
Shalaka Gajanayake <shalakagajanayake123@gmail.com>
2025-08-01 05:56:35 +0530

    updated 3

--- pages/header/header.php
+++ pages/header/header.php
@@ -68,21 +68,13 @@
                                 </ul> -->
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">About</a>
+                                <a class="nav-link" href="our-services.html" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="about-us.html">About Us</a></li>
-                                    <li><a class="dropdown-item" href="our-team.html">Our Team</a></li>
-                                    <li><a class="dropdown-item" href="team-single.html">Team Single</a></li>
+                                    <!-- <li><a class="dropdown-item" href="our-services.html">Our Services</a></li> -->
+                                    <!-- <li><a class="dropdown-item" href="service-single.html">Services Single</a></li> -->
                                 </ul>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="our-services.html">Our Services</a></li>
-                                    <li><a class="dropdown-item" href="service-single.html">Services Single</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
+                            <!-- <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="contact.html">Contact Us</a></li>
@@ -91,25 +83,60 @@
                                     <li><a class="dropdown-item" href="coming-soon.html">Coming soon</a></li>
                                     <li><a class="dropdown-item" href="404-page.html">404 Page</a></li>
                                 </ul>
-                            </li>
+                            </li> -->
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Portfolio</a>
-                                <ul class="dropdown-menu">
+                                <a class="nav-link" href="portfolio-2col.html" aria-expanded="false">Portfolio</a>
+                                <!-- <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="portfolio-2col.html">Portfolio 2 Column</a></li>
                                     <li><a class="dropdown-item" href="portfolio-3col.html">Portfolio 3 Column</a></li>
                                     <li><a class="dropdown-item" href="portfolio-4col.html">Portfolio 4 Column</a></li>
                                     <li><a class="dropdown-item" href="portfolio-single.html">Portfolio Single</a></li>
+                                </ul> -->
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">About</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="about-us.html">About Us</a></li>
+                                    <li><a class="dropdown-item" href="our-team.html">Our Team</a></li>
+                                    <!-- <li><a class="dropdown-item" href="team-single.html">Team Single</a></li> -->
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
+                                <a class="nav-link" href="faqs.html" aria-expanded="false">FAQs</a>
+                                <!-- <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="portfolio-2col.html">Portfolio 2 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-3col.html">Portfolio 3 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-4col.html">Portfolio 4 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-single.html">Portfolio Single</a></li>
+                                </ul> -->
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="contact.html" aria-expanded="false">Contact Us</a>
+                                <!-- <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="portfolio-2col.html">Portfolio 2 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-3col.html">Portfolio 3 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-4col.html">Portfolio 4 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-single.html">Portfolio Single</a></li>
+                                </ul> -->
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="coming-soon.html" aria-expanded="false">Coming Soon</a>
+                                <!-- <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="portfolio-2col.html">Portfolio 2 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-3col.html">Portfolio 3 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-4col.html">Portfolio 4 Column</a></li>
+                                    <li><a class="dropdown-item" href="portfolio-single.html">Portfolio Single</a></li>
+                                </ul> -->
+                            </li>
+                            <!-- <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="blog-grid.html">Blog Grid</a></li>
                                     <li><a class="dropdown-item" href="blog-standard.html">Blog Standard</a></li>
                                     <li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
                                 </ul>
-                            </li>
-                            <li class="nav-item dropdown">
+                            </li> -->
+                            <!-- <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Elements</a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="typography.html#heading">Headings Styles</a></li>
@@ -122,7 +149,7 @@
                                     <li><a class="dropdown-item" href="typography.html#forms-styles">Forms Styles</a></li>
                                     <li><a class="dropdown-item" href="typography.html#pagination-styles">Pagination Styles</a></li>
                                 </ul>
-                            </li>
+                            </li> -->
                         </ul>
                     </div>
                 </div>
